ebc249f563d8dc19eb088513290ddcc060eabeca
adya <myish1084@gmail.com>
2023-06-13 21:35:06 +0300

    Ex 001

---
+++ Ex001/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат (число, умноженное на само себя) 
+
+Console.WriteLine("Введите целое число: ");
+string number = Console.ReadLine()!;
+int num = int.Parse(number);
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} равен {square}");
+
+// Console.WriteLine(num * num);
