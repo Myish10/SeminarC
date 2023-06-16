465153ea9f3878e91df7935d882be694144894b0
adya <myish1084@gmail.com>
2023-06-16 10:58:36 +0300

    Ex 008

---
+++ Ex008/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+int InputNum (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+string Quart (int num)
+{
+    if (num == 1 ) return "Точка находится в диапазоне x > 0, y > 0";
+    else if (num == 2) return "Точка находится в диапазоне x < 0, y > 0";
+    else if (num == 3) return "Точка находится в диапазоне x < 0, y < 0";
+    else if (num == 4) return "Точка находится в диапазоне x > 0, y < 0";
+    else return "Такой четверти не существует";
+}
+
+int x = InputNum("Введите номер четверти: ");
+
+string res = Quart(x);
+
+Console.WriteLine(res);
