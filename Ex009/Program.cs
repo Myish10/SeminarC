54e713a66810e1b9e8f5542ac1f8363a66f8ede9
adya <myish1084@gmail.com>
2023-06-18 17:15:38 +0300

    Ex 009 1

--- Ex009/Program.cs
+++ Ex009/Program.cs
@@ -2,23 +2,24 @@
 // d = √((хА – хВ)2 + (уА – уВ)2   Корень суммы квадратов разностей координат
 // Math.Pow(value, degree)-возвращает только double
 // Math.Sqrt(value)
-double InputNum (string message)
+double InputNum(string message)
 {
     Console.Write(message);
     return double.Parse(Console.ReadLine()!);
 }
 
-double Distance (double a1, double a2, double b1, double b2)
+double Distance(double a1, double a2, double b1, double b2)
 {
-double sum = Math.Pow((a1 - b1), 2) + Math.Pow((a2 - b2), 2);
-double res = Math.Sqrt(sum);
-return res;
+    // double sum = Math.Pow((a1 - b1), 2) + Math.Pow((a2 - b2), 2);
+    // double res = Math.Sqrt(sum);
+    double res = Math.Sqrt((Math.Pow((a1 -b1), 2) + Math.Pow((a2 - b2), 2)));
+    return res;
 }
 
-double num1 = InputNum("Введите координату X точки 1: ");
-double num2 = InputNum("Введите координату Y точки 1: ");
-double num3 = InputNum("Введите координату X точки 2: ");
-double num4 = InputNum("Введите координату Y точки 2: ");
+double numa1 = InputNum("Введите координату X точки 1: ");
+double numa2 = InputNum("Введите координату Y точки 1: ");
+double numb1 = InputNum("Введите координату X точки 2: ");
+double numb2 = InputNum("Введите координату Y точки 2: ");
 
-double result = Math.Round((Distance(num1, num2, num3, num4)), 2);
+double result = Math.Round((Distance(numa1, numa2, numb1, numb2)), 2);
 Console.WriteLine($"Расстояние между точками 1 и 2 = {result}");
