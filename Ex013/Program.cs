2e7ca4fa5148cd8136b538974ebaaa7c5e0dee83
adya <myish1084@gmail.com>
2023-06-18 17:57:43 +0300

    Ex 013

---
+++ Ex013/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+ int number = InputNum("Введите целое число: ");
+
+ for (int i = 1; i <= number; i++)
+ {
+    Console.Write($"{Math.Pow(i, 2)}");
+    if (i < number)
+        Console.Write(", ");
+    else
+        Console.Write(".");
+ }
