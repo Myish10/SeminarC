1ae4a1a02b4f2a42e151d23877ccd1a425c493f3
adya <myish1084@gmail.com>
2023-06-20 11:41:32 +0300

    Ex 016

---
+++ Ex016/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+ int Factorial(int num)
+ {
+    int multipl = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        multipl = multipl*i;
+    }
+    return multipl;
+ }
+ int num = InputNum("Введите целое число: ");
+ int fact = Factorial(num);
+ Console.WriteLine(fact);
