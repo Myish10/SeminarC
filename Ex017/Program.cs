a42d0fd99315fecbad8f991387a2461dac2b2a1e
adya <myish1084@gmail.com>
2023-06-20 11:41:46 +0300

    Ex 017

---
+++ Ex017/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int[] ArrayRand(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(2);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+}
+int len = InputNum("Введите длину массива: ");
+int[] arr = ArrayRand(len);
+PrintArray(arr);
