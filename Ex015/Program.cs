ef29a0747b2893f7ffee47a471b19d1c6504a9ab
adya <myish1084@gmail.com>
2023-06-20 11:41:21 +0300

    Ex 015

---
+++ Ex015/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int DigitNum(int num)
+{
+    int index = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        index++;
+    }
+    return index;
+}
+int num = InputNum("Введите целое число: ");
+int number = DigitNum(num);
+Console.WriteLine($"В числе {num} {number} цифр");
