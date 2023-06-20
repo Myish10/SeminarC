b208bc917ef9109976751ee4644c388dbabf5b44
adya <myish1084@gmail.com>
2023-06-20 11:41:08 +0300

    Ex 014

---
+++ Ex014/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+int SumFrom1ToN(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i; // sum = sum + i
+    }
+    return sum;
+}
+
+int InputNum (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int num = InputNum("Введите целое число N: ");
+int sum = SumFrom1ToN(num);
+Console.WriteLine($"Сумма чисел от 1 до N равна {sum}.");
