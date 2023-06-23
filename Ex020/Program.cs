80bbcd8400efd014e2c3b2ba35d4306313667257
adya <myish1084@gmail.com>
2023-06-23 12:34:29 +0300

    Ex 020

---
+++ Ex020/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] array, int min, int max)
+{
+    Random rnd = new Random(); // создание экземпляра класса Random
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(min, max + 1); // [min, max) полуинтервал
+    // array[i] = new Random().Next(min, max + 1)
+}
+string PrintArray(int[] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+        res += array[i] + " ";
+    return res;
+}
+
+void ParNum(int[] array, int num)
+{
+    int length = array.Length;
+    int index = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] == num)
+        {
+           index++;
+        } 
+    }
+    if  (index > 0) Console.WriteLine("Присутствует");
+    else Console.WriteLine("Такого числа нет");
+}
+
+int size = InputNum("Введите размер массива: ");
+int minValue = InputNum("Введите минимальное значение элемента: ");
+int maxValue = InputNum("Введите максимальное значение элемента: ");
+int num = InputNum("Введите искомое число: ");
+
+int[] myArray = CreateArray(size);
+FillArray(myArray, minValue, maxValue);
+string txt = PrintArray(myArray);
+Console.WriteLine(txt);
+
+ParNum(myArray, num);
