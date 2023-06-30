b0ab3a552b6b7c0f7082e5ecfbc2b9e1e4c6ddc9
adya <myish1084@gmail.com>
2023-06-30 16:43:25 +0300

    Ex 023

---
+++ Ex023/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void ReversArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+int pos = InputNum("Введите длину массива: ");
+int minValue = InputNum("Введите  минимальное число массива: ");
+int maxValue = InputNum("Введите  максимальное число массива: ");
+int[] myArray = CreateArray(pos);
+FillArray(myArray, minValue, maxValue);
+PrintArray(myArray);
+ReversArray(myArray);
+PrintArray(myArray);
+
