28d9b7578e94276cbb6464eff74d618e29af0278
adya <myish1084@gmail.com>
2023-06-30 16:44:05 +0300

    Ex 026

---
+++ Ex026/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10 Решение с помощью строки!!!
+
+Console.WriteLine("Введите целое число: ");
+int num = int.Parse(Console.ReadLine()!);
+string count = "";
+while (num > 0)
+{
+    count = num % 2 + count;
+    num /= 2;
+}
+Console.WriteLine(count);
