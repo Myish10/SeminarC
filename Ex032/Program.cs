a4d6dc38774417a0ebcb4f11234ed6310d4ad760
adya <myish1084@gmail.com>
2023-07-05 13:26:48 +0300

    Ex 032

---
+++ Ex032/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] Create2DArray(int rows, int cols)
+{
+    return new int[rows, cols];
+}
+
+void Fill2DArray(int[,] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max + 1);
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+void ChangeRows(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        (array[0, j], array[array.GetLength(0) - 1, j]) = (array[array.GetLength(0) - 1, j], array[0, j]); 
+    }
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+int minValue = InputNum("Введите минимальное значение диапазона: ");
+int maxValue = InputNum("Введите максимальное значение диапазона: ");
+
+int[,] my2DArray = Create2DArray(rows, columns);
+Fill2DArray(my2DArray, minValue, maxValue);
+Print2DArray(my2DArray);
+Console.WriteLine();
+ChangeRows(my2DArray);
+Print2DArray(my2DArray);
