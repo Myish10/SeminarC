8f71b5b3b6ac14bf25be78f02434bd4fa361135e
adya <myish1084@gmail.com>
2023-07-09 17:57:26 +0300

    Ex 038

---
+++ Ex038/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumDigits(int num)
+{
+    if ( num == 0) return 0;
+    return num % 10 + SumDigits(num / 10);
+}
+
+int number = InputNum("Введите целое число: ");
+int result = SumDigits(number);
+Console.WriteLine(result);
