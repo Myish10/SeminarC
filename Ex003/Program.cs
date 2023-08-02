0d2c222d09d873ce65d57987636054542b604f46
adya <myish1084@gmail.com>
2023-08-02 12:42:50 +0300

    Ex 003.1

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -20,10 +20,10 @@ a = -3 b = 9 -> нет
 // }
 
 Console.Write("Введите первое число: ");
-int num1 = int.Parse(Console.ReadLine());
+int num1 = int.Parse(Console.ReadLine()!);
 
 Console.Write("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine()!);
 
 if (num1 == num2 * num2)
 {
