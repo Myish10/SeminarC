6f54007f922a9f16c915c7d2748847f491d951e3
adya <myish1084@gmail.com>
2023-08-02 12:45:33 +0300

    Ex 041

---
+++ Ex041_SortVibor/Program.cs
@@ -0,0 +1,33 @@
+﻿// Сортировка выбором
+void InputArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+    }
+}
+
+int[] SortVibor(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[min])
+                min = j;
+        }
+        int temp = array[min];
+        array[min] = array[i];
+        array[i] = temp;
+    }
+    return array;
+}
+
+
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Конечный массив: [{string.Join(", ", SortVibor(array))}]");
