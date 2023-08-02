2ce317428e0bf9df8e19c5a285354fefbec0b097
adya <myish1084@gmail.com>
2023-08-02 12:43:22 +0300

    Ex 005.1

--- Ex005/Program.cs
+++ Ex005/Program.cs
@@ -3,7 +3,7 @@
 // 	782 -> 2
 // 	918 -> 8
 Console.WriteLine("ВВедите трехзначное число");
-// int number = Convert.ToInt32(Console.ReadLine());
-int number = int.Parse(Console.ReadLine());
+// int number = Convert.ToInt32(Console.ReadLine()!);
+int number = int.Parse(Console.ReadLine()!);
 int rem = number % 10;
 Console.WriteLine(rem);
