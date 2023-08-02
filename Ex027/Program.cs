4bb05c5881cabb9edf082a03789d8d869678ca8d
adya <myish1084@gmail.com>
2023-08-02 12:44:09 +0300

    Ex 027.1

--- Ex027/Program.cs
+++ Ex027/Program.cs
@@ -20,7 +20,7 @@ void Fill2DArray(int[,] array, int min, int max)
 
 void Print2DArray(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+   for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
             Console.Write($"{array[i, j]}\t");
