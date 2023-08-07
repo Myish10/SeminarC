31936c0fd91445bac9edd5bbf18366556937bade
adya <myish1084@gmail.com>
2023-08-07 17:01:07 +0300

    Ex 042

---
+++ EX042_SortBable/Program.cs
@@ -0,0 +1,30 @@
+﻿// Сортировка выбором
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+    }
+}
+
+int[] SortBable(int[] array, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            int temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+        }
+    }
+    return array;
+}
+
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Конечный массив: [{string.Join(", ", SortBable(array, n))}]");
